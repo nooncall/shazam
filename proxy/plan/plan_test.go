XiaoMi/Gaea

bug fix:wrong database name in unshard mode (#80)

proxy/plan/plan_test.go
@@ -19,6 +19,7 @@ import (
 	"sync/atomic"
 	"testing"
 
+	"github.com/XiaoMi/Gaea/backend"
 	"github.com/XiaoMi/Gaea/models"
 	"github.com/XiaoMi/Gaea/parser"
 	"github.com/XiaoMi/Gaea/proxy/router"
@@ -40,8 +41,9 @@ type OrderSequence struct {
 }
 
 type PlanInfo struct {
-	rt   *router.Router
-	seqs *sequence.SequenceManager
+	phyDBs map[string]string
+	rt     *router.Router
+	seqs   *sequence.SequenceManager
 }
 
 func NewOrderSequence(db, table, pkName string) *OrderSequence {
@@ -73,7 +75,7 @@ func getTestFunc(info *PlanInfo, test SQLTestcase) func(t *testing.T) {
 			t.Fatalf("parse sql error: %v", err)
 		}
 
-		p, err := BuildPlan(stmt, test.db, test.sql, info.rt, info.seqs)
+		p, err := BuildPlan(stmt, info.phyDBs, test.db, test.sql, info.rt, info.seqs)
 		if err != nil {
 			if test.hasErr {
 				t.Logf("BuildPlan got expect error, sql: %s, err: %v", test.sql, err)
@@ -95,11 +97,13 @@ func getTestFunc(info *PlanInfo, test SQLTestcase) func(t *testing.T) {
 		case *ExplainPlan:
 			actualSQLs = plan.sqls
 		case *UnshardPlan:
-			if test.hasErr {
-				t.Logf("find unshard plan in shard testing")
-				return
+			actualSQLs = make(map[string]map[string][]string)
+			dbSQLs := make(map[string][]string)
+			if db, ok := info.phyDBs[plan.db]; ok {
+				plan.db = db
 			}
-			t.Fatalf("find unshard plan in shard testing")
+			dbSQLs[plan.db] = []string{plan.sql}
+			actualSQLs[backend.DefaultSlice] = dbSQLs
 		}
 
 		if actualSQLs == nil {
@@ -490,8 +494,9 @@ func preparePlanInfo() (*PlanInfo, error) {
 	}
 
 	planInfo := &PlanInfo{
-		rt:   rt,
-		seqs: seqs,
+		phyDBs: nsModel.DefaultPhyDBS,
+		rt:     rt,
+		seqs:   seqs,
 	}
 	return planInfo, nil
 }

proxy/plan/plan_unshard.go
@@ -16,10 +16,12 @@ package plan
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/XiaoMi/Gaea/backend"
 	"github.com/XiaoMi/Gaea/mysql"
 	"github.com/XiaoMi/Gaea/parser/ast"
+	"github.com/XiaoMi/Gaea/parser/format"
 	"github.com/XiaoMi/Gaea/util"
 )
 
@@ -27,9 +29,10 @@ import (
 type UnshardPlan struct {
 	basePlan
 
-	db   string
-	sql  string
-	stmt ast.StmtNode
+	db     string
+	phyDBs map[string]string
+	sql    string
+	stmt   ast.StmtNode
 }
 
 // SelectLastInsertIDPlan is the plan for SELECT LAST_INSERT_ID()
@@ -65,14 +68,35 @@ func IsSelectLastInsertIDStmt(stmt ast.StmtNode) bool {
 }
 
 // CreateUnshardPlan constructor of UnshardPlan
-func CreateUnshardPlan(stmt ast.StmtNode, db, sql string) *UnshardPlan {
+func CreateUnshardPlan(stmt ast.StmtNode, phyDBs map[string]string, db string, tableNames []*ast.TableName) (*UnshardPlan, error) {
 	p := &UnshardPlan{
-		db:   db,
-		sql:  sql,
-		stmt: stmt,
+		db:     db,
+		phyDBs: phyDBs,
+		stmt:   stmt,
 	}
+	rewriteUnshardTableName(phyDBs, tableNames)
+	rsql, err := generateUnshardingSQL(stmt)
+	if err != nil {
+		return nil, fmt.Errorf("generate unshardPlan SQL error: %v", err)
+	}
+	p.sql = rsql
+	return p, nil
+}
 
-	return p
+func rewriteUnshardTableName(phyDBs map[string]string, tableNames []*ast.TableName) {
+	for _, tableName := range tableNames {
+		if phyDB, ok := phyDBs[tableName.Schema.String()]; ok {
+			tableName.Schema.O = phyDB
+			tableName.Schema.L = strings.ToLower(phyDB)
+		}
+	}
+}
+
+func generateUnshardingSQL(stmt ast.StmtNode) (string, error) {
+	s := &strings.Builder{}
+	ctx := format.NewRestoreCtx(format.EscapeRestoreFlags, s)
+	_ = stmt.Restore(ctx)
+	return s.String(), nil
 }
 
 // CreateSelectLastInsertIDPlan constructor of SelectLastInsertIDPlan

proxy/plan/plan_unshard_test.go
@@ -1,0 +1,49 @@
+package plan
+
+import (
+	"testing"
+
+	"github.com/XiaoMi/Gaea/backend"
+)
+
+func TestUnshardPlan(t *testing.T) {
+	ns, err := preparePlanInfo()
+	if err != nil {
+		t.Fatalf("prepare namespace error: %v", err)
+	}
+	tests := []SQLTestcase{
+		{
+			db:  "db_mycat",
+			sql: `select * from tbl_unshard_a as a join db_mycat.tbl_unshard_b as b on a.id = b.id`,
+			sqls: map[string]map[string][]string{
+				backend.DefaultSlice: {
+					"db_mycat_0": {"SELECT * FROM `tbl_unshard_a` AS `a` JOIN `db_mycat_0`.`tbl_unshard_b` AS `b` ON `a`.`id`=`b`.`id`"},
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.sql, getTestFunc(ns, test))
+	}
+}
+
+func TestUnshardPlanWithoutDB(t *testing.T) {
+	ns, err := preparePlanInfo()
+	if err != nil {
+		t.Fatalf("prepare namespace error: %v", err)
+	}
+	tests := []SQLTestcase{
+		{
+			db:  "db_mycat",
+			sql: `select * from tbl_unshard limit 10`,
+			sqls: map[string]map[string][]string{
+				backend.DefaultSlice: {
+					"db_mycat_0": {"SELECT * FROM `tbl_unshard` LIMIT 10"},
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.sql, getTestFunc(ns, test))
+	}
+}

nooncall/shazam

proxy/plan/plan_test.go
@@ -19,6 +19,7 @@ import (
 	"sync/atomic"
 	"testing"
 
+	"github.com/nooncall/shazam/backend"
 	"github.com/nooncall/shazam/models"
 	"github.com/nooncall/shazam/parser"
 	"github.com/nooncall/shazam/proxy/router"
@@ -40,8 +41,9 @@ type OrderSequence struct {
 }
 
 type PlanInfo struct {
-	rt   *router.Router
-	seqs *sequence.SequenceManager
+	phyDBs map[string]string
+	rt     *router.Router
+	seqs   *sequence.SequenceManager
 }
 
 func NewOrderSequence(db, table, pkName string) *OrderSequence {
@@ -73,7 +75,7 @@ func getTestFunc(info *PlanInfo, test SQLTestcase) func(t *testing.T) {
 			t.Fatalf("parse sql error: %v", err)
 		}
 
-		p, err := BuildPlan(stmt, test.db, test.sql, info.rt, info.seqs)
+		p, err := BuildPlan(stmt, info.phyDBs, test.db, test.sql, info.rt, info.seqs)
 		if err != nil {
 			if test.hasErr {
 				t.Logf("BuildPlan got expect error, sql: %s, err: %v", test.sql, err)
@@ -95,11 +97,13 @@ func getTestFunc(info *PlanInfo, test SQLTestcase) func(t *testing.T) {
 		case *ExplainPlan:
 			actualSQLs = plan.sqls
 		case *UnshardPlan:
-			if test.hasErr {
-				t.Logf("find unshard plan in shard testing")
-				return
+			actualSQLs = make(map[string]map[string][]string)
+			dbSQLs := make(map[string][]string)
+			if db, ok := info.phyDBs[plan.db]; ok {
+				plan.db = db
 			}
-			t.Fatalf("find unshard plan in shard testing")
+			dbSQLs[plan.db] = []string{plan.sql}
+			actualSQLs[backend.DefaultSlice] = dbSQLs
 		}
 
 		if actualSQLs == nil {
@@ -469,8 +473,9 @@ func preparePlanInfo() (*PlanInfo, error) {
 	}
 
 	planInfo := &PlanInfo{
-		rt:   rt,
-		seqs: seqs,
+		phyDBs: nsModel.DefaultPhyDBS,
+		rt:     rt,
+		seqs:   seqs,
 	}
 	return planInfo, nil
 }

proxy/plan/plan_unshard.go
@@ -16,10 +16,12 @@ package plan
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/nooncall/shazam/backend"
 	"github.com/nooncall/shazam/mysql"
 	"github.com/nooncall/shazam/parser/ast"
+	"github.com/nooncall/shazam/parser/format"
 	"github.com/nooncall/shazam/util"
 )
 
@@ -27,9 +29,10 @@ import (
 type UnshardPlan struct {
 	basePlan
 
-	db   string
-	sql  string
-	stmt ast.StmtNode
+	db     string
+	phyDBs map[string]string
+	sql    string
+	stmt   ast.StmtNode
 }
 
 // SelectLastInsertIDPlan is the plan for SELECT LAST_INSERT_ID()
@@ -65,14 +68,35 @@ func IsSelectLastInsertIDStmt(stmt ast.StmtNode) bool {
 }
 
 // CreateUnshardPlan constructor of UnshardPlan
-func CreateUnshardPlan(stmt ast.StmtNode, db, sql string) *UnshardPlan {
+func CreateUnshardPlan(stmt ast.StmtNode, phyDBs map[string]string, db string, tableNames []*ast.TableName) (*UnshardPlan, error) {
 	p := &UnshardPlan{
-		db:   db,
-		sql:  sql,
-		stmt: stmt,
+		db:     db,
+		phyDBs: phyDBs,
+		stmt:   stmt,
 	}
+	rewriteUnshardTableName(phyDBs, tableNames)
+	rsql, err := generateUnshardingSQL(stmt)
+	if err != nil {
+		return nil, fmt.Errorf("generate unshardPlan SQL error: %v", err)
+	}
+	p.sql = rsql
+	return p, nil
+}
 
-	return p
+func rewriteUnshardTableName(phyDBs map[string]string, tableNames []*ast.TableName) {
+	for _, tableName := range tableNames {
+		if phyDB, ok := phyDBs[tableName.Schema.String()]; ok {
+			tableName.Schema.O = phyDB
+			tableName.Schema.L = strings.ToLower(phyDB)
+		}
+	}
+}
+
+func generateUnshardingSQL(stmt ast.StmtNode) (string, error) {
+	s := &strings.Builder{}
+	ctx := format.NewRestoreCtx(format.EscapeRestoreFlags, s)
+	_ = stmt.Restore(ctx)
+	return s.String(), nil
 }
 
 // CreateSelectLastInsertIDPlan constructor of SelectLastInsertIDPlan

proxy/plan/plan_unshard_test.go
@@ -1,0 +1,49 @@
+package plan
+
+import (
+	"testing"
+
+	"github.com/nooncall/shazam/backend"
+)
+
+func TestUnshardPlan(t *testing.T) {
+	ns, err := preparePlanInfo()
+	if err != nil {
+		t.Fatalf("prepare namespace error: %v", err)
+	}
+	tests := []SQLTestcase{
+		{
+			db:  "db_mycat",
+			sql: `select * from tbl_unshard_a as a join db_mycat.tbl_unshard_b as b on a.id = b.id`,
+			sqls: map[string]map[string][]string{
+				backend.DefaultSlice: {
+					"db_mycat_0": {"SELECT * FROM `tbl_unshard_a` AS `a` JOIN `db_mycat_0`.`tbl_unshard_b` AS `b` ON `a`.`id`=`b`.`id`"},
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.sql, getTestFunc(ns, test))
+	}
+}
+
+func TestUnshardPlanWithoutDB(t *testing.T) {
+	ns, err := preparePlanInfo()
+	if err != nil {
+		t.Fatalf("prepare namespace error: %v", err)
+	}
+	tests := []SQLTestcase{
+		{
+			db:  "db_mycat",
+			sql: `select * from tbl_unshard limit 10`,
+			sqls: map[string]map[string][]string{
+				backend.DefaultSlice: {
+					"db_mycat_0": {"SELECT * FROM `tbl_unshard` LIMIT 10"},
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.sql, getTestFunc(ns, test))
+	}
+}